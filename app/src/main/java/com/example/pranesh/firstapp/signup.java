1e4406bfcd7857daa225bf5d8ebe2310e10147e0
PPranesh <ppranesh5@gmail.com>
2018-03-21 23:58:18 +0530

    SignUp and SignIn page.

---
+++ app/src/main/java/com/example/pranesh/firstapp/signup.java
@@ -0,0 +1,46 @@
+package com.example.pranesh.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Pranesh on 21-Mar-18.
+ */
+
+public class signup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+    public void onSignUpClick(View v) {
+        if (v.getId() == R.id.Bsignupbutton) {
+            EditText name = (EditText) findViewById(R.id.TFname);
+            EditText email = (EditText) findViewById(R.id.TFemail);
+            EditText uname = (EditText) findViewById(R.id.TFusername);
+            EditText pass1 = (EditText) findViewById(R.id.TFpass1);
+            EditText pass2 = (EditText) findViewById(R.id.TFpass2);
+
+            String namestr = name.getText().toString();
+            String emailstr = email.getText().toString();
+            String unamestr = uname.getText().toString();
+            String pass1str = pass1.getText().toString();
+            String pass2str = pass2.getText().toString();
+
+            if (!pass1str.equals(pass2str)) {
+                Toast pass = Toast.makeText(signup.this, "Password don't match!!", Toast.LENGTH_SHORT);
+                pass.show();
+            }
+        }
+
+    }
+
+
+}
+
+
